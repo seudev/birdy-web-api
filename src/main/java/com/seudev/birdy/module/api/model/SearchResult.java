7cd1bf4e07af4baa7504a8ec19d8ea32f760b938
Thomás Sousa Silva (ThomasSousa96) <thomas@seudev.com>
2020-12-15 23:33:27 -0300

    feat(api): add a empty() method in the SearchResult
    Add a SearchResult.empty() method to facilitate the class usage.

--- src/main/java/com/seudev/birdy/module/api/model/SearchResult.java
+++ src/main/java/com/seudev/birdy/module/api/model/SearchResult.java
@@ -1,19 +1,31 @@
 package com.seudev.birdy.module.api.model;
 
+import static java.util.Collections.emptyList;
+
 import java.util.Collection;
 import java.util.Optional;
 
 public class SearchResult<E> {
 
+    private static final SearchResult<?> EMPTY_SEARCH_RESULT = new SearchResult<>(emptyList(), 0L);
+
     private final Optional<Long> count;
 
     private final Collection<E> records;
 
+    @SuppressWarnings("unchecked")
+    public static <E> SearchResult<E> empty() {
+        return (SearchResult<E>) EMPTY_SEARCH_RESULT;
+    }
+
     public static <E> SearchResult<E> from(Collection<E> records) {
-        return new SearchResult<>(records, null);
+        return from(records, null);
     }
 
     public static <E> SearchResult<E> from(Collection<E> records, Long count) {
+        if (records.isEmpty() && ((count == null) || (count == 0L))) {
+            return empty();
+        }
         return new SearchResult<>(records, count);
     }
 
